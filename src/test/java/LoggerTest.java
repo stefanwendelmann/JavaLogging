696ecea8b9d0bf735116b6b564925771206db122
Stefan Wendelmann <SWendelmann@itout.de>
2019-05-02 16:09:17 +0200

    Make a Single test Update Clean for Database Use the same db in pom.xml and log4j2-test.xml

--- src/test/java/LoggerTest.java
+++ src/test/java/LoggerTest.java
@@ -1,9 +1,12 @@
 
-
 import java.io.*;
 import java.nio.charset.*;
+import java.sql.*;
+import java.util.*;
 import java.util.concurrent.*;
 import org.apache.logging.log4j.*;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.core.*;
 import org.apache.logging.log4j.core.appender.*;
 import org.apache.logging.log4j.core.config.*;
@@ -19,6 +22,54 @@ public class LoggerTest
 
   private static final org.apache.logging.log4j.Logger logg = LogManager.getLogger();
   private static final LoggerContext ctx = (LoggerContext) LogManager.getContext();
+  private static final String CHECKMD5 = "9f15ae8af42d9cddd35a69f9958ce73d";
+  private Properties mvnProperties;
+  private Connection con = null;
+
+  @Before
+  public void before() throws IOException, ClassNotFoundException, SQLException
+  {
+    // Load Maven Properties written by the properties-maven-plugin to get DB infos
+    java.io.InputStream is = this.getClass().getResourceAsStream("mvn.properties");
+    java.util.Properties p = new Properties();
+    p.load(is);
+
+    // Load DB Connection 
+    Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+    con = DriverManager.getConnection(
+      p.getProperty("sql.url.test") + ";database=" + p.getProperty("sql.db.test"),
+      p.getProperty("sql.user.test"),
+      p.getProperty("sql.pw.test"));
+  }
+
+  @After
+  public void after() throws SQLException
+  {
+    if (con != null)
+    {
+      con.close();
+    }
+  }
+
+  @Test
+  public void testSingle() throws InterruptedException, SQLException
+  {
+    Logger log = LogManager.getLogger("Testlogger");
+
+    ThreadContext.put("schnittstelle", "SingleTest");
+    ThreadContext.put("version", "S01");
+    ThreadContext.put("laufid", "S00");
+
+    log.info("testSingle" + CHECKMD5);
+    // Wait for Log4j to ASYNC log the message to the DB
+    // May Fail due high latency!
+    Thread.sleep(1_000L);
+
+    Statement s = con.createStatement();
+    ResultSet rs = s.executeQuery("SELECT * FROM LAUFLOG WHERE TEXT = 'testSingle" + CHECKMD5 + "'");
+
+    Assert.assertTrue(rs.next());
+  }
 
   @Test
   public void testProfiling() throws InterruptedException
@@ -108,7 +159,6 @@ public class LoggerTest
       ThreadContext.put("schnittstelle", "LISA4711");
       ThreadContext.put("version", "L01");
       ThreadContext.put("laufid", laufId);
-      ThreadContext.put("user", "SPADMIN");
 
       for (int j = 0; j < logs; j++)
       {
